88aba54013a114ff9510a694c78ef1acc0da5d9e
hy0by0 <hy0by02792c3@gmail.com>
2026-09-12 04:05:16 +0900

    クレジットのアイコン追加、アカウントURL、誤字修正
    クレジットへアイコンを追加。
    それぞれのURLへ飛ばす機能の追加
    一部誤字の修正

---
+++ Assets/Scripts/UI/AccessWeb.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class AccessWeb : MonoBehaviour
+{
+    public string url; // URLを指定する変数
+    public void clickWeb()
+    {
+        Application.OpenURL(url);
+    }
+}
